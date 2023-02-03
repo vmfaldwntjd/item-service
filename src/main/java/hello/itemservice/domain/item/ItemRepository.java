beea59c991edaadf8a29ec96897b34d18500b24b
core <jinjs6@gmail.com>
2023-02-03 20:32:52 +0900

    feat: 아이템 레퍼지토리 파일 생성

--- src/main/java/hello/itemservice/domain/item/ItemRepository.java
+++ src/main/java/hello/itemservice/domain/item/ItemRepository.java
@@ -1,5 +1,13 @@
 package hello.itemservice.domain.item;
 
+import org.springframework.stereotype.Repository;
 
+import java.util.HashMap;
+import java.util.Map;
+
+@Repository
 public class ItemRepository {
+
+    private static final Map<Long, Item> store = new HashMap<>();
+    private static long sequence = 0L;
 }
