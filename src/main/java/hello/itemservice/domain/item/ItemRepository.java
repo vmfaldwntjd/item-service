efe9503493f23779af6838591ba453c224f3b6e1
core <jinjs6@gmail.com>
2023-02-03 20:51:09 +0900

    feat: 아이템 찾기, 조회, 업데이트 기능 작성

--- src/main/java/hello/itemservice/domain/item/ItemRepository.java
+++ src/main/java/hello/itemservice/domain/item/ItemRepository.java
@@ -2,8 +2,11 @@ package hello.itemservice.domain.item;
 
 import org.springframework.stereotype.Repository;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 
 @Repository
 public class ItemRepository {
@@ -15,5 +18,21 @@ public class ItemRepository {
         item.setId(++sequence);
         store.put(item.getId(), item);
         return item;
+    }
+
+    public Item findById(Long id) {
+        return store.get(id);
+    }
+
+    public List<Item> findAll() {
+        return new ArrayList<>(store.values());
+    }
+
+    public void update(Long itemId, Item updateParam) {
+        Item findItem = findById(itemId);
+        findItem.setItemName(updateParam.getItemName());
+        findItem.setPrice(updateParam.getPrice());
+        findItem.setQuantity(updateParam.getQuantity());
+
     }
 }
