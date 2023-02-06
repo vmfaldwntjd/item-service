9c110365d818008d793b4fcdbea7314c004e4400
core <jinjs6@gmail.com>
2023-02-06 21:10:16 +0900

    refactor: 테스트 관련 init 메서드 삭제

--- src/main/java/hello/itemservice/web/form/FormController.java
+++ src/main/java/hello/itemservice/web/form/FormController.java
@@ -60,14 +60,4 @@ public class FormController {
         itemRepository.update(itemId, item);
         return "redirect:/form/items/{itemId}";
     }
-
-    /**
-     * 테스트용 데이터 추가
-     */
-    @PostConstruct
-    public void init() {
-
-        itemRepository.save(new Item("itemA", 10000, 10));
-        itemRepository.save(new Item("itemB", 20000, 20));
-    }
 }
